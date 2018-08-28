29e1b3b09a2c0dd340d78a4b70e90f9bde79f6d2
Ran Bi <biran@mit.edu>
2018-08-28 15:50:18 +0200

    add akpu4calo jet sequence, patched for missing parameters

--- HeavyIonsAnalysis/JetAnalysis/python/FullJetSequence_XeXe_mc.py
+++ HeavyIonsAnalysis/JetAnalysis/python/FullJetSequence_XeXe_mc.py
@@ -9,6 +9,7 @@ ak3PFJets = ak4PFJets.clone(rParam = 0.3)
 from RecoJets.JetProducers.ak4GenJets_cfi import ak4GenJets
 ak5GenJets = ak4GenJets.clone(rParam = 0.4)
 
+from HeavyIonsAnalysis.JetAnalysis.jets.HiRecoJets_cff import *
 from HeavyIonsAnalysis.JetAnalysis.jets.HiRecoPFJets_cff import *
 #from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
 #from RecoHI.HiJetAlgos.hiFJRhoProducer import hiFJRhoProducer
@@ -65,6 +66,7 @@ from HeavyIonsAnalysis.JetAnalysis.jets.ak3PFJetSequence_pp_mc_cff import *
 from HeavyIonsAnalysis.JetAnalysis.jets.ak4PFJetSequence_pp_mc_cff import *
 from HeavyIonsAnalysis.JetAnalysis.jets.ak5PFJetSequence_pp_mc_cff import *
 from HeavyIonsAnalysis.JetAnalysis.jets.ak4CaloJetSequence_pp_mc_cff import *
+from HeavyIonsAnalysis.JetAnalysis.jets.akPu4CaloJetSequence_pp_mc_cff import *
 from HeavyIonsAnalysis.JetAnalysis.jets.akPu4PFJetSequence_pp_mc_cff import *
 from HeavyIonsAnalysis.JetAnalysis.jets.akCs4PFJetSequence_pp_mc_cff import *
 from HeavyIonsAnalysis.JetAnalysis.jets.akSoftDrop4PFJetSequence_pp_mc_cff import *
@@ -83,6 +85,7 @@ jetSequences = cms.Sequence(
     kt4PFJetsForRho*hiFJRhoProducer +
     hiFJGridEmptyAreaCalculator +
     PFTowers +
+    akPu4CaloJets +
     akPu4PFJets +
     akCs4PFJets +
     akSoftDrop4PFJets +
@@ -92,6 +95,43 @@ jetSequences = cms.Sequence(
     ak4PFJetSequence +
 #    ak4CaloJetSequence +
 #    akSoftDrop4PFJetSequence +
+    akPu4CaloJetSequence +
     akPu4PFJetSequence +
     akCs4PFJetSequence
 )
+
+# temporary fix for akPu4CaloJetAnalyzer
+akPu4CaloJetAnalyzer.doExtendedFlavorTagging = cms.untracked.bool(False)
+
+akPu4CaloJetAnalyzer.trackPairV0Filter = cms.PSet(
+    k0sMassWindow = cms.double(0.05)
+    )
+
+akPu4CaloJetAnalyzer.trackSelection = cms.PSet(
+    a_dR = cms.double(-0.001053),
+    a_pT = cms.double(0.005263),
+    b_dR = cms.double(0.6263),
+    b_pT = cms.double(0.3684),
+    jetDeltaRMax = cms.double(0.3),
+    maxDecayLen = cms.double(99999.9),
+    maxDistToAxis = cms.double(0.2),
+    max_pT = cms.double(500),
+    max_pT_dRcut = cms.double(0.1),
+    max_pT_trackPTcut = cms.double(3),
+    min_pT = cms.double(120),
+    min_pT_dRcut = cms.double(0.5),
+    normChi2Max = cms.double(99999.9),
+    pixelHitsMin = cms.uint32(2),
+    ptMin = cms.double(1.0),
+    qualityClass = cms.string('any'),
+    sip2dSigMax = cms.double(99999.9),
+    sip2dSigMin = cms.double(-99999.9),
+    sip2dValMax = cms.double(99999.9),
+    sip2dValMin = cms.double(-99999.9),
+    sip3dSigMax = cms.double(99999.9),
+    sip3dSigMin = cms.double(-99999.9),
+    sip3dValMax = cms.double(99999.9),
+    sip3dValMin = cms.double(-99999.9),
+    totalHitsMin = cms.uint32(8),
+    useVariableJTA = cms.bool(False)
+    )
