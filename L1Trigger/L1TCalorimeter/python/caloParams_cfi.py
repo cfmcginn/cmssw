edd7ecee06b6c3d33e47b090b283cd589599ba78
Christopher McGinn <cfmcginn@mit.edu>
2023-08-07 18:44:08 +0200

    First pass at adding ZDC LUT handling to CaloParamsHelper.h, L1TCaloStage2ParamsESProducer.cc, and caloParams_cfi.py

--- L1Trigger/L1TCalorimeter/python/caloParams_cfi.py
+++ L1Trigger/L1TCalorimeter/python/caloParams_cfi.py
@@ -142,7 +142,9 @@ caloParams = cms.ESProducer(
     minimumBiasThresholds = cms.vint32(0, 0, 0, 0),
     centralityLUTFile = cms.FileInPath("L1Trigger/L1TCalorimeter/data/centralityLUT_stage1.txt"),
     q2LUTFile         = cms.FileInPath("L1Trigger/L1TCalorimeter/data/q2LUT_stage1.txt"),
-
+    #When zdc lut is ready put it here
+    #    zdcLUTFile        = cms.FileInPath("L1Trigger/L1TCalorimeter/data/"),
+    
     # HCal FB LUT
     layer1HCalFBLUTUpper = cms.vuint32([
     0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 
