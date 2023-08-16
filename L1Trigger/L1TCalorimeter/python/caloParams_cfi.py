114986c0c4e02128c8f16a61facc044571c5c46b
Elisa Fontanesi <elfontan@lxplus.cern.ch>
2023-08-16 22:11:59 +0200

    Fix to run emulation with cmsDriver command

--- L1Trigger/L1TCalorimeter/python/caloParams_cfi.py
+++ L1Trigger/L1TCalorimeter/python/caloParams_cfi.py
@@ -143,8 +143,9 @@ caloParams = cms.ESProducer(
     centralityLUTFile = cms.FileInPath("L1Trigger/L1TCalorimeter/data/centralityLUT_stage1.txt"),
     q2LUTFile         = cms.FileInPath("L1Trigger/L1TCalorimeter/data/q2LUT_stage1.txt"),
     #When zdc lut is ready put it here
-    #    zdcLUTFile        = cms.FileInPath("L1Trigger/L1TCalorimeter/data/"),
-    
+    #For now we are using the q2LUT as a dummy file (parameter does need a file for code to run)
+    zdcLUTFile        = cms.FileInPath("L1Trigger/L1TCalorimeter/data/q2LUT_stage1.txt"),
+
     # HCal FB LUT
     layer1HCalFBLUTUpper = cms.vuint32([
     0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 0xBBBABBBA, 
